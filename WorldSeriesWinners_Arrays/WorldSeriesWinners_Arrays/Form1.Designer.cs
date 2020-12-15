2a472394c3d5924466bd592c4d5ebfce3d5fa2ef
Carrancs <58002272+Carrancs@users.noreply.github.com>
2020-12-15 02:59:20 -0500

    Add files via upload

---
+++ WorldSeriesWinners_Arrays/WorldSeriesWinners_Arrays/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WorldSeriesWinners_Arrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.findOutBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.teamsListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 51);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select one of the teams in the list below \r\nand find out how many times it has wo" +
+    "n\r\n the World Series between 1903 and 2012";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // findOutBtn
+            // 
+            this.findOutBtn.Location = new System.Drawing.Point(24, 312);
+            this.findOutBtn.Name = "findOutBtn";
+            this.findOutBtn.Size = new System.Drawing.Size(108, 34);
+            this.findOutBtn.TabIndex = 4;
+            this.findOutBtn.Text = "Find Out!";
+            this.findOutBtn.UseVisualStyleBackColor = true;
+            this.findOutBtn.Click += new System.EventHandler(this.findOutBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(187, 312);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(108, 34);
+            this.exitBtn.TabIndex = 4;
+            this.exitBtn.Text = "Close";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // teamsListBox
+            // 
+            this.teamsListBox.FormattingEnabled = true;
+            this.teamsListBox.Location = new System.Drawing.Point(50, 75);
+            this.teamsListBox.Name = "teamsListBox";
+            this.teamsListBox.Size = new System.Drawing.Size(208, 212);
+            this.teamsListBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 358);
+            this.Controls.Add(this.teamsListBox);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.findOutBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "World Series Winners";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button findOutBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.ListBox teamsListBox;
+    }
+}
+
---
+++ WorldSeriesWinners_Arrays/WorldSeriesWinners_Arrays/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WorldSeriesWinners_Arrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // *************** METHODS ***************
+
+        // My own search method
+        private int RepeatedTimes(string[] wArray, string listSel)
+        {
+            // Variables
+            int matchTimes = 0; // Variable to hold win times total
+
+            int roundit = 0;  // Variable to use on the while loop of the through the Array
+
+            string userSel;    // Variable to hold the total times the team has won
+
+            if (teamsListBox.SelectedIndex != -1)
+            {
+                // Determine how many times the item is in the array
+                while (roundit < wArray.Length)
+                {
+                    userSel = wArray[roundit];
+                    if (listSel == userSel)
+                    {
+                        matchTimes++;
+                    }
+                    roundit++;
+                }
+            }
+            return matchTimes;
+        }
+
+        // *************** LOAD LIST BOX ***************
+        // Load the ListBox with the Teams
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Display the items read from the file "teams.txt" into the ListBox
+                // Declare the variables
+                string team;      // To display the teams as String on the List Box
+                int totOfNum;     // To hold the total of items (numbers) on the List Box
+
+                // Open a File
+                StreamReader inputFile;
+
+                // Open the selected File
+                inputFile = File.OpenText("teams.txt");
+
+                // Clear the List Box
+                teamsListBox.Items.Clear();
+
+                // Read the file's content
+                while (!inputFile.EndOfStream)
+                {
+                    // Get each team from the file
+                    team = inputFile.ReadLine();
+
+                    // Add each team to the List Box
+                    //  teamsListBox.Items.Add(inputFile.ReadLine());
+                    teamsListBox.Items.Add(team);
+
+                }
+
+                // Close the file
+                inputFile.Close();
+
+                // Get the total of items (numbers) stored on the List Box
+                totOfNum = teamsListBox.Items.Count;
+                //MessageBox.Show(totOfNum.ToString());
+            }
+
+            catch (Exception ex)
+            {
+                //Display an error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void findOutBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // *************** ARRAY ***************
+                // Create an array to hold items read from the file
+                const int SIZE = 108;                     // Constant to hold the size of the Array
+                string[] winnerTeams = new string[SIZE];  
+
+                int listIndex = 0; // Counter variable to use in the loop
+
+                // Declare a StreamReader variable
+                StreamReader inputFile;
+
+                // Open the file and get StreamReader Object
+                inputFile = File.OpenText("WorldSeriesWinners.txt");
+
+                // Read the file's content into the array
+                while (!inputFile.EndOfStream)
+                {
+                    winnerTeams[listIndex] = inputFile.ReadLine().ToString();
+                    listIndex++;
+                }
+
+                // Close the file
+                inputFile.Close();
+
+                // ************* WIN TIMES *************
+
+                // Get the selected item
+                string selection = teamsListBox.SelectedItem.ToString();
+                //MessageBox.Show(selection);
+
+                int winTimes;
+
+                winTimes = RepeatedTimes(winnerTeams, selection);
+
+                // Display the message box with the winning times result
+                MessageBox.Show("The " + selection + " have won " + winTimes.ToString() + " times the World Series");
+
+            }
+
+            catch (Exception ex)
+            {
+                //Display an error message
+                MessageBox.Show("ERROR: " + ex.Message);
+            }
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            // Cose the form
+            this.Close();
+        }
+    }
+}
